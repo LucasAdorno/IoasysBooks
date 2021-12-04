8d2d29dd3d80aab7e1e6aa4fdcbcf75103d101ab
LucasAdorno <lucasadorno2702@gmail.com>
2021-12-04 13:36:14 -0300

    feat: add white background to colors theme

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -2,6 +2,7 @@ export default {
   colors: {
     background: '#E5E5E5',
     white_text: '#FFFFFF',
+    white_background: '#FFFFFF',
     input_background: 'rgba(0, 0, 0, 0.32)',
     vine: '#B22E6F',
   },
